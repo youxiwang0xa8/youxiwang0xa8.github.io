4bf7a5486038f28b26bf1071f363eeb21e4e369e
yangqi.a8 <yangqi.a8@bytedance.com>
2022-12-12 00:04:48 +0800

    fix
    Change-Id: Ibef85f975578b02b2504eb2e5fc8bb4fe7225eee

--- app.js
+++ app.js
@@ -1,4 +1,14 @@
 function onYolo() {
     console.log('on Yolo')
-    document.getElementById('nice-yolo').style.display='block'
+    document.getElementById('modal-yolo').style.display='block'
+}
+
+function onContact() {
+    console.log('on Contact')
+    document.getElementById('modal-contact').style.display='block'
+}
+
+function onMenu() {
+    console.log('on Menu')
+    document.getElementById('modal-menu').style.display='block'
 }
