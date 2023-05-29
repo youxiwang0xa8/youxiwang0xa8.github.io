bc7678114051ffd68c3923d1a532a770785b2b86
youxiwang0xa8 <116572898+youxiwang0xa8@users.noreply.github.com>
2023-05-29 05:30:53 +0000

    dataLayer push yolo_shopping

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 function onYolo() {
     console.log('on Yolo')
+    dataLayer.push({'event': 'yolo_shopping'}) // to trigger Tag-yolo_shopping
     document.getElementById('modal-yolo').style.display='block'
 }
 
